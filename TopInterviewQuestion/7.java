98dc12a6490f06e940e282e963e7b4bb1051521f
Ahmet Furkan Sevim <ahmetfurkansevim5.gmail.com>
2022-07-22 16:39:02 +0300

    Top interview question 7

---
+++ TopInterviewQuestion/7.java
@@ -0,0 +1,41 @@
+https://leetcode.com/problems/reverse-integer/
+
+class Solution {
+    int reverse(int x) {
+        if(x == -2147483648){
+            return 0;
+        }
+        boolean flag = false;
+        
+        if(x < 0){
+            flag = true;
+            x = -x;
+        }
+
+        int sum = 0;
+        int digit = 1;
+        int temp = x;
+
+        while (x / 10 > 0){
+            digit++;
+            x = x / 10;
+        }
+
+        digit--;
+
+        while (temp / 10 > 0){
+            double curr = (temp % 10 * Math.pow(10. , (double) digit));
+            if(sum + curr > 2147483647){
+                return 0;
+            }
+            sum += (int) curr;
+            temp = temp / 10;
+            digit--;
+        }
+
+        sum += (int) (temp % 10 * Math.pow(10. , (double) digit));
+
+        return flag?-sum:sum;
+    }
+
+}
